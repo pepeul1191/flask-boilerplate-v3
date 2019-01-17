69cdc6ca80f2d5b18e03a7671bba44ceaaa54c80
Pepe Valdivia <jvaldivia@softweb.pe>
2019-01-17 17:44:40 -0500

    Renderizando vista con layout

--- demo/views.py
+++ demo/views.py
@@ -2,12 +2,21 @@
 # -*- coding: utf-8 -*-
 import datetime
 from flask import Blueprint, render_template, request, session, redirect
-# from main.constants import constants
+from main.constants import constants
 # from main.middlewares import session_false, session_true
 # from .helpers import index_css, index_js
 
 demo_view = Blueprint('demo_view', __name__)
 
 @demo_view.route('/demo', methods=['GET'])
+#@session_false
 def index():
-  return render_template('demo/index.html')
+  locals = {
+    'title': 'Bienvenido',
+    'message': '',
+    'constants': constants,
+    #'csss': index_css(),
+    #'jss': index_js(),
+  }
+  print(locals)
+  return render_template('demo/index.html', locals = locals)
